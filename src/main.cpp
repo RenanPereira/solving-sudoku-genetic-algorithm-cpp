769d585086608168ffe35a2ddee63e028b550889
Renan Pereira <renan.the.physicist@gmail.com>
2026-07-03 17:26:48 +0100

    improvements to the main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 // Renan Câmara Pereira
-// Solving a sudoku game in C++
+// Solving sudoku puzzles in C++ using the genetic algorithm
 
 #include <iostream>
 #include <vector>
@@ -12,61 +12,39 @@ int main(void)
     //srand((unsigned) time(0));
 	srand(1);
 
-	
 	double start_s = omp_get_wtime();
-
-	/*
-	SudokuSkelleton sudokuUnsolved = SudokuSkelleton(9,"sudoku_example1.dat");
-
-	sudokuUnsolved.toString();
-
-	std::cout << "# of clues: " << sudokuUnsolved.numberOfClues() << "\n";
-
-	SudokuAnsatz sudoku1(sudokuUnsolved);
-
-	std::cout << "\n";
-	sudoku1.toString();
 	
-	std::cout << sudoku1.getWeight() << "\n";
-	*/
 
-
-	std::vector< std::vector<int> > sudoku_to_solve;
-
-	sudoku_to_solve = Sudoku::read_from_file(9, "sudoku_example1.dat"); 
+	std::vector< std::vector<int> > sudoku_to_solve = Sudoku::read_from_file(9, "sudoku_example1.dat"); 
 
 	Sudoku::print(sudoku_to_solve);
 
 	std::cout << Sudoku::number_of_clues(sudoku_to_solve) << "\n";
 
-	//generate sudoku population
-	std::vector< std::vector< std::vector<int> > > sudoku_population;
+	//parameters to solve sudoku with genetic algorithm
 	int N = 50;
 	int N_births_from_vacuum = 10;
 	int N_descendents = 20;
+	int generations = 350;
 
-
+	//generate sudoku population
 	std::vector< std::vector< std::vector<int> > > sudoku_pop = Sudoku::generate_population(N, sudoku_to_solve);
 
-
-	int generations = 350;
 	for (int i = 0; i < generations; ++i)
 	{
 		std::cout << i << "\t" << sudoku_pop.size() << "\n";
-
-
 
 		for (int j = 0; j < N_births_from_vacuum; ++j)
 		{
 			sudoku_pop.push_back( Sudoku::generate_by_lines(sudoku_to_solve) );
 		}
+
 		//mutate some individuals
 		for (int j = 0; j < int(sudoku_pop.size()); ++j)
 		{
 			int X = ( rand() % sudoku_pop.size() );
 			sudoku_pop[X] = Sudoku::mutate(sudoku_to_solve, sudoku_pop[X], 0.25);
 		}
-
 
 		//generate descendents
 		std::vector< std::vector< std::vector<int> > > sudoku_pop_descendents;
@@ -77,11 +55,7 @@ int main(void)
 			sudoku_pop_descendents[j] = Sudoku::mutate(sudoku_to_solve, sudoku_pop_descendents[j], 1.0);
 		}
 
-
-
 		sudoku_pop = Sudoku::survival_of_the_fittest(sudoku_pop, sudoku_pop_descendents, N);
-
-
 
 		int neo_weight = Sudoku::weight_configuration(sudoku_pop[0]);
 
@@ -89,6 +63,7 @@ int main(void)
 		if (neo_weight==0){ break; }
 	
 	}
+	
 	Sudoku::print(sudoku_pop[0]);
 
 
